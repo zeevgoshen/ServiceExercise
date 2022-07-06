633427875c4ba8de9e683012a89cd1989efc1f74
Ze'ev Goshen <zeevgoshen@gmail.com>
2022-07-06 20:32:20 +0300

    blocking

--- ServiceExercise/RequestsService.cs
+++ ServiceExercise/RequestsService.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ServiceExercise
@@ -13,7 +15,8 @@ namespace ServiceExercise
         static int maxConnections = 0;
         private static Connection connection = new Connection();
         private static int currentConnectionsNumber = 0;
-
+        private static int _sum = 0;
+        private static ConcurrentBag<Request> requests = null;
 
         public static RequestsService Instance
         {
@@ -41,25 +44,96 @@ namespace ServiceExercise
         {
             maxConnections = _MaxConnections;
         }
+
+        private RequestsService(IAsyncEnumerable<Request> stream)
+        {
+            
+        }
+
+
+        private static async IAsyncEnumerable<string> GenerateNames()
+        {
+            yield return "Anurag";
+            yield return "Pranaya";
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            yield return "Sambit";
+        }
+
+
         public int getSummary()
         {
-            throw new NotImplementedException();
+             return _sum;
         }
 
         // should not block
         public void sendRequest(Request request)
         {
+            List<Connection> connections = new List<Connection>(maxConnections);
 
-            //startRunAsyncTask(request);Task<int> result = 
+            //requests = new ConcurrentBag<Request>();
 
-            int number = 0;
-            while (currentConnectionsNumber < maxConnections)
-            {
-                //connection.runCommand(request.Command);
-                number += runAsyncTask(request).Result;
+
+            // Task.Run(() => {
+            //    requests.Add(request);
+
+            //});
+            //while (request != null)
+            //{
+            //    requests.Add(request);
+            //}
+
+            int i = 0;
+            while (i < maxConnections) {
+                connections.Add(new Connection());
+                i++;
             }
 
+            foreach (var connection in connections)
+            {
+                Connect(connection, request);
+            }
+            //startRunAsyncTask(request);Task<int> result = 
 
+            //int number = 0;
+            //while (currentConnectionsNumber < maxConnections)
+            //{
+            //    //connection.runCommand(request.Command);
+            //    number += runAsyncTask(request).Result;
+            //}
+
+
+        }
+
+        public static async void Connect(Connection connection, Request request)
+        {
+            var tasks = new List<Task<int>>();
+            //int sum = 0;
+
+            await Task.Run(() =>
+            {
+                //Interlocked.Add(ref sum, connection.runCommand(request.Command));
+                var response = ProcessCard(connection, request);
+                tasks.Add(response);
+            });
+
+            //It will execute all the tasks concurrently
+            await Task.WhenAll(tasks);
+        }
+
+         
+
+        public static async Task<int> ProcessCard(Connection connection, Request request)
+        {   
+
+            Action action = () => { Interlocked.Add(ref _sum, connection.runCommand(request.Command)); };
+            Task t1 = new Task(action);
+            
+            //await Task.Delay(1000);
+            //await t1.Start();
+            //string message = $"Credit Card Number: {creditCard.CardNumber} Name: {creditCard.Name} Processed";
+            await Task.Run(action);
+            string message = $"Credit Card Number: {_sum } Processed";
+            return _sum;
         }
 
         //private int startRunAsyncTask(Request request)
@@ -74,19 +148,47 @@ namespace ServiceExercise
 
         //}
 
+        public static async Task Foo(int sum, Request request)
+        {
+            Console.WriteLine("Thread {0} - Start {1}", Thread.CurrentThread.ManagedThreadId, sum);
+
+            //await Task.Delay(1000);
+            Action action = () => { Interlocked.Add(ref sum, connection.runCommand(request.Command)); };
+            Task t1;
+            t1 = new Task(action);
+
+            await Task.Run(() => t1.Start());
+            //t1.Start();
+
+            Console.WriteLine("Thread {0} - End {1}", Thread.CurrentThread.ManagedThreadId, sum);
+        }
+
         private async Task<int> runAsyncTask(Request request)
         {
 
             try
             {
+                int sum = 0;
+                Task t1 = null;
+                var tasks = new List<Task>();
+
+                //Action action = () => { Interlocked.Add(ref sum,connection.runCommand(request.Command)); };
+
+                //Action action = () => { Interlocked.Add(ref sum, connection.runCommand(request.Command)); };
 
 
-                await Task.Run(() =>
+
+                //t1 = new Task(action);
+
+                while (currentConnectionsNumber < maxConnections)
                 {
-                    return connection.runCommand(request.Command);
-                });
+                    tasks.Add(Foo(sum, request));              
+                    //await t1.Start();
+                    currentConnectionsNumber++;
+                }
 
-                return Task.Result;
+                Task.WaitAll(tasks.ToArray());
+                return sum;
 
             }
             catch (Exception ex)
